5f47a2760c68e3ae7a640f09b3edd6b74a4a43f9
ciro-stodulski <ciro.sda@gmail>
2023-09-25 17:30:44 -0300

    feat(git-actions): pr block

---
+++ pr-block/pull-request-comment-branch-main/src/main.ts
@@ -0,0 +1,33 @@
+import { getInput, setFailed, setOutput } from "@actions/core";
+
+import { isPullRequest, pullRequestDetails } from "./PullRequests";
+
+export async function run() {
+  try {
+    const token = getInput("repo_token", { required: true });
+
+    if (!isPullRequest(token)) {
+      throw Error("Comment is not on a pull request");
+    }
+
+    const {
+      base_ref,
+      base_sha,
+      head_ref,
+      head_sha,
+    } = await pullRequestDetails(token);
+
+    setOutput("base_ref", base_ref);
+    setOutput("base_sha", base_sha);
+    setOutput("head_ref", head_ref);
+    setOutput("head_sha", head_sha);
+  } catch (error) {
+    if (error instanceof Error) {
+      setFailed(error.message);
+    } else {
+      throw error;
+    }
+  }
+}
+
+run();
